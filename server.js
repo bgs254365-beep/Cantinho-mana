1045058a8a115c87161e5061897052ecd123669b
bgs254365-beep <bgs254365@gmail.com>
2026-09-02 17:36:39 -0300

    Correção de api
    conectando a api a o projeto

--- server.js
+++ server.js
@@ -5,28 +5,30 @@ const app = express();
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({
-    mensagem: "API do Cantinho do Maná funcionando!"
-  });
+    res.json({
+        mensagem: "API do Cantinho do Maná funcionando!"
+    });
 });
 
 app.get("/api/produtos", (req, res) => {
-  res.json([
-    {
-      id: 1,
-      nome: "Rei Davi",
-      preco: 10,
-      categoria: "Hambúrgueres"
-    },
-    {
-      id: 2,
-      nome: "Elias",
-      preco: 11,
-      categoria: "Hambúrgueres"
-    }
-  ]);
+    res.json([{
+            id: 1,
+            nome: "Rei Davi",
+            preco: 10,
+            categoria: "Hambúrgueres"
+        },
+        {
+            id: 2,
+            nome: "Elias",
+            preco: 11,
+            categoria: "Hambúrgueres"
+        }
+    ]);
 });
 
-app.listen(3001, () => {
-  console.log("API rodando em http://localhost:3001");
+
+const PORT = process.env.PORT || 3001; // Usa a porta da nuvem ou a 3001 local
+
+app.listen(PORT, () => {
+    console.log(`API rodando na porta ${PORT}`);
 });
